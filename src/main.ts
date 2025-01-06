662e7215553854c6915ace5a236244d08687d42d
Alejandro Luna <alejandro2002.uar@gmail.com>
2025-01-06 03:38:37 -0300

    Refactoring main

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document); // Ruta para Swagger: http://localhost:3000/api-docs
 
-  await app.listen(3000);
-
 
   await app.listen(process.env.PORT ?? 3000);
 }
